d264f2d275a05cf2d20d06fd22d552dbb234e572
Pedro Antonio S <111252527+PA-PedroAntonio@users.noreply.github.com>
2024-05-10 09:29:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,17 @@ const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-05-10T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1;
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minustos / 60);
+    let dias = Math.floor(horas / 24);
+    return dias;
+}
+
+contadores[0].textContent = Date("2024-05-20T00:00:00");
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
